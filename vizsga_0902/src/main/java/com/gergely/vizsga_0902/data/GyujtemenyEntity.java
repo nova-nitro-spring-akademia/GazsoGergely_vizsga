42e63087bd65a866140b92134c9fe2251791bf97
gergelygazso <66570469+gergelygazso@users.noreply.github.com>
2022-09-02 12:12:08 +0200

    10 done: GyujtemenyEntity letrehozva

---
+++ vizsga_0902/src/main/java/com/gergely/vizsga_0902/data/GyujtemenyEntity.java
@@ -0,0 +1,43 @@
+package com.gergely.vizsga_0902.data;
+
+import com.gergely.vizsga_0902.service.Card;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class GyujtemenyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    String name;
+
+    @OneToMany
+    Set<CardEntity> cards;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<CardEntity> getCards() {
+        return cards;
+    }
+
+    public void setCards(Set<CardEntity> cards) {
+        this.cards = cards;
+    }
+}
